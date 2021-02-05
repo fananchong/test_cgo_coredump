e0147f2c5401f809d6f95dbc68563fd5784e3dee
fananchong <fananchong@126.com>
2021-02-05 16:04:45 +0800

    up

--- catch_except.go
+++ catch_except.go
@@ -9,14 +9,32 @@ package main
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <limits.h>
 
-extern void onExcept(int signum);
+static size_t get_executable_path( char* processdir,char* processname, size_t len)
+{
+	char* path_end;
+	if(readlink("/proc/self/exe", processdir,len) <=0)
+		return -1;
+	path_end = strrchr(processdir,  '/');
+	if(path_end == NULL)
+		return -1;
+	++path_end;
+	strcpy(processname, path_end);
+	*path_end = '\0';
+	return (size_t)(path_end - processdir);
+}
 
 static void handler(int signum) {
+	printf("crash signum:%d\n", signum);
 	char cmd[50];
 	sprintf(cmd, "gcore %u", getpid());
 	system(cmd);
-	onExcept(signum);
+	char path[PATH_MAX];
+	char processname[1024];
+	get_executable_path(path, processname, sizeof(path));
+	sprintf(cmd, "./gdb_print.sh ./%s ./core.%u", processname, getpid());
+	system(cmd);
 	exit(1);
 }
 
@@ -30,56 +48,7 @@ static void __attribute__ ((constructor)) sigsetup(void) {
 }
 */
 import "C"
-import (
-	"bufio"
-	"fmt"
-	"io"
-	"os"
-	"os/exec"
-	"strings"
-)
-
-//export onExcept
-func onExcept(signum C.int) {
-	fmt.Println("crash, signum:", signum)
-
-	// 打印 Crash 堆栈
-	result := execCommand("./gdb_bt.sh", []string{"./main2", fmt.Sprintf("./core.%d", os.Getpid())})
-	for _, line := range result {
-		fmt.Printf(line)
-	}
-	fmt.Println("")
-}
 
 func sigsetup() {
 	C.sigsetup()
-}
-
-func execCommand(commandName string, params []string) (result []string) {
-	cmd := exec.Command(commandName, params...)
-	//显示运行的命令
-	fmt.Printf("执行命令: %s\n", strings.Join(cmd.Args, " "))
-	pipe, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "error=>", err.Error())
-		return nil
-	}
-	cmd.Start() // Start开始执行c包含的命令，但并不会等待该命令完成即返回。Wait方法会返回命令的返回状态码并在命令返回后释放相关的资源。
-
-	reader := bufio.NewReader(pipe)
-
-	var index int
-	//实时循环读取输出流中的一行内容
-	for {
-		line, err2 := reader.ReadString('\n')
-		if err2 != nil || io.EOF == err2 {
-			break
-		}
-		result = append(result, line)
-		index++
-	}
-
-	cmd.Wait()
-
-	return result
 }
